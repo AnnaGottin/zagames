1559cb456cfeef19ae775667364f1834b6aa7af2
AnnaGottin <shabyanna@gmail.com>
2022-02-23 07:36:51 -0600

    Next Demo
    new URL

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -25,10 +25,10 @@ export default function Content(){
                             </div>
                             <div className="mt-3 sm:mt-0 sm:ml-3">
                             <a
-                                href="#"
+                                href="https://gamerlandpro.000webhostapp.com/"
                                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                             >
-                                Buy Demo
+                                Next Demo
                             </a>
                             </div>
                         </div>
